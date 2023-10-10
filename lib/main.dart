f06df1f1f2479728b61cba97b8c1ea31670b3434
ssstttaaasssiiikk <ssstttaaasssiiikk@gmail.com>
2023-10-10 16:36:22 +0400

    add (toolbar&appbar): add toolbar&appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'componets/navbar.dart';
-import 'componets/appbar.dart';
+
+import 'package:merlin/componets/appbar.dart';
+import 'package:merlin/componets/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,23 +9,40 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: CustomAppBar(),
-      bottomNavigationBar: CustomNavBar()
-    );
+        appBar: CustomAppBar(),
+        bottomNavigationBar: CustomNavBar(),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ])));
   }
 }
