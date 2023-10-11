c244413fd997f39eb09efe766a6c8913a28ce372
Bogdan Eremin <55001168+kixygod@users.noreply.github.com>
2023-10-11 20:16:46 +0700

    fix(settings): added dark theme switch

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -27,29 +27,85 @@ class MySettings extends StatelessWidget {
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
   @override
+  // ignore: library_private_types_in_public_api
   _SettingsPageState createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool darkBackground = false;
-  Color textColor = Colors.black;
+  // Переменные для темы
+  bool darkThemeBackground = false;
+  Color themeAppBackground = MyColors.bgWhite;
+  Color themeBackground = MyColors.white;
+  Color themeTextColor = MyColors.black;
+  Color themeGrayTextColor = MyColors.grey;
+  // Перменная для изменения темы
   bool isChecked = false;
+  // Переменные для изменений цвета предпросмотра
+  bool backgroundColorBlack = false;
+  bool backgroundColorWhite = false;
+  bool backgroundColorMint = false;
+  bool backgroundColorBeige = false;
+  Color backgroundColorPreview = MyColors.mint;
+  bool textColorBlack = false;
+  bool textColorWhite = false;
+  bool textColorMint = false;
+  bool textColorBeige = false;
+  Color textColorPreview = MyColors.black;
+
+  void updateBackgroundColor(Color newColor) {
+    backgroundColorPreview = newColor;
+  }
+
+  void updateTextColor(Color newColor) {
+    textColorPreview = newColor;
+  }
+
+  void updateTheme() {
+    if (isChecked) {
+      // Включена темная тема
+      darkThemeBackground = true;
+      themeAppBackground = MyColors.blackGray;
+      themeBackground = MyColors.darkGray;
+      themeTextColor = MyColors.white;
+      themeGrayTextColor = MyColors.white;
+      ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        background: MyColors.darkGray,
+      ));
+      // Обновите остальные переменные темной темы при необходимости
+    } else {
+      // Включена светлая тема
+      darkThemeBackground = false;
+      themeAppBackground = MyColors.white;
+      themeBackground = MyColors.bgWhite;
+      themeTextColor = Colors.black;
+      themeGrayTextColor = MyColors.grey;
+      ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        background: MyColors.white,
+      ));
+      // Обновите остальные переменные светлой темы при необходимости
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: darkThemeBackground ? themeBackground : themeBackground,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: MyColors.white,
+        backgroundColor: themeAppBackground,
         leading: SvgPicture.asset(
           'assets/images/chevron-left.svg',
           width: 16,
           height: 16,
         ),
-        title: const Text(
+        title: Text(
           'Настройки',
           style: TextStyle(
-            color: MyColors.black,
+            color: darkThemeBackground ? themeTextColor : themeTextColor,
             fontFamily: 'Tektur',
             fontSize: 16,
           ),
@@ -65,36 +121,41 @@ class _SettingsPageState extends State<SettingsPage> {
       body: Column(
         children: [
           Container(
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(255, 255, 255, 1),
+            decoration: BoxDecoration(
+              color: themeBackground,
             ),
             padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                const TextTektur(
+                TextTektur(
                   text: "Система",
                   fontsize: 18,
-                  textColor: MyColors.black,
+                  textColor: themeTextColor,
                 ),
                 const SizedBox(height: 16),
                 Row(
                   children: <Widget>[
-                    const TextTektur(
+                    TextTektur(
                       text: "Ночной режим",
                       fontsize: 14,
-                      textColor: MyColors.grey,
+                      textColor: themeGrayTextColor,
                     ),
                     const Spacer(),
                     CustomCheckbox(
                       isChecked: isChecked,
-                      bgColor: MyColors.white,
-                      borderColor: MyColors.black,
-                      checkColor: MyColors.puple,
+                      bgColor: darkThemeBackground
+                          ? themeAppBackground
+                          : themeAppBackground,
+                      borderColor:
+                          darkThemeBackground ? themeTextColor : themeTextColor,
+                      checkColor:
+                          darkThemeBackground ? themeTextColor : themeTextColor,
                       onChanged: (newValue) {
                         setState(() {
                           isChecked = newValue;
+                          updateTheme();
                         });
                       },
                     ),
@@ -104,18 +165,19 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
           ),
           Container(
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(255, 255, 255, 1),
+            margin: const EdgeInsets.only(top: 8.0),
+            decoration: BoxDecoration(
+              color: themeBackground,
             ),
             padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                const TextTektur(
+                TextTektur(
                   text: "Настраиваемая тема",
                   fontsize: 18,
-                  textColor: MyColors.black,
+                  textColor: themeTextColor,
                 ),
                 const SizedBox(height: 16),
                 Row(
@@ -125,54 +187,70 @@ class _SettingsPageState extends State<SettingsPage> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        const TextTektur(
+                        TextTektur(
                           text: "Цвет текста",
                           fontsize: 14,
-                          textColor: MyColors.grey,
+                          textColor: themeGrayTextColor,
                         ),
                         Row(
                           children: [
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: textColorBlack,
                               bgColor: MyColors.black,
                               borderColor: MyColors.black,
                               checkColor: MyColors.white,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  textColorBlack = newValue;
+                                  textColorWhite = false;
+                                  textColorMint = false;
+                                  textColorBeige = false;
+                                  updateTextColor(MyColors.black);
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: textColorWhite,
                               bgColor: MyColors.white,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  textColorBlack = false;
+                                  textColorWhite = newValue;
+                                  textColorMint = false;
+                                  textColorBeige = false;
+                                  updateTextColor(MyColors.white);
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: textColorMint,
                               bgColor: MyColors.mint,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  textColorBlack = false;
+                                  textColorWhite = false;
+                                  textColorMint = newValue;
+                                  textColorBeige = false;
+                                  updateTextColor(MyColors.mint);
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: textColorBeige,
                               bgColor: MyColors.beige,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  textColorBlack = false;
+                                  textColorWhite = false;
+                                  textColorMint = false;
+                                  textColorBeige = newValue;
+                                  updateTextColor(MyColors.beige);
                                 });
                               },
                             ),
@@ -183,54 +261,70 @@ class _SettingsPageState extends State<SettingsPage> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        const TextTektur(
+                        TextTektur(
                           text: "Цвет фона",
                           fontsize: 14,
-                          textColor: MyColors.grey,
+                          textColor: themeGrayTextColor,
                         ),
                         Row(
                           children: [
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: backgroundColorBlack,
                               bgColor: MyColors.black,
                               borderColor: MyColors.black,
                               checkColor: MyColors.white,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  updateBackgroundColor(MyColors.black);
+                                  backgroundColorBlack = newValue;
+                                  backgroundColorWhite = false;
+                                  backgroundColorMint = false;
+                                  backgroundColorBeige = false;
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: backgroundColorWhite,
                               bgColor: MyColors.white,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  updateBackgroundColor(MyColors.white);
+                                  backgroundColorBlack = false;
+                                  backgroundColorWhite = newValue;
+                                  backgroundColorMint = false;
+                                  backgroundColorBeige = false;
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: backgroundColorMint,
                               bgColor: MyColors.mint,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  updateBackgroundColor(MyColors.mint);
+                                  backgroundColorBlack = false;
+                                  backgroundColorWhite = false;
+                                  backgroundColorMint = newValue;
+                                  backgroundColorBeige = false;
                                 });
                               },
                             ),
                             CustomCheckbox(
-                              isChecked: isChecked,
+                              isChecked: backgroundColorBeige,
                               bgColor: MyColors.beige,
                               borderColor: MyColors.black,
                               checkColor: MyColors.black,
                               onChanged: (newValue) {
                                 setState(() {
-                                  isChecked = newValue;
+                                  updateBackgroundColor(MyColors.beige);
+                                  backgroundColorBlack = false;
+                                  backgroundColorWhite = false;
+                                  backgroundColorMint = false;
+                                  backgroundColorBeige = newValue;
                                 });
                               },
                             ),
@@ -241,7 +335,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   ],
                 ),
                 Padding(
-                    padding: EdgeInsets.only(top: 16),
+                    padding: const EdgeInsets.only(top: 16),
                     child: Container(
                         width: MediaQuery.of(context).size.width,
                         height: 56,
@@ -251,15 +345,15 @@ class _SettingsPageState extends State<SettingsPage> {
                               child: Container(
                                   width: MediaQuery.of(context).size.width,
                                   height: 56,
-                                  decoration: const BoxDecoration(
-                                    color: Color.fromRGBO(203, 223, 206, 1),
+                                  decoration: BoxDecoration(
+                                    color: backgroundColorPreview,
                                   ))),
-                          const Center(
+                          Center(
                               child: Text(
                             'Тестовый текст темы',
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              color: MyColors.black,
+                              color: textColorPreview,
                               fontFamily: 'Roboto',
                               fontSize: 14,
                               fontWeight:
@@ -276,71 +370,5 @@ class _SettingsPageState extends State<SettingsPage> {
         ],
       ),
     );
-  }
-
-  Widget _buildColorSettingsSection() {
-    return Column(
-      children: [
-        _buildSettingOption(
-          'Фон белый',
-          darkBackground,
-          Colors.white,
-        ),
-        _buildSettingOption(
-          'Фон черный',
-          !darkBackground,
-          Colors.black,
-        ),
-        _buildSettingOption(
-          'Текст чёрный',
-          textColor == Colors.black,
-          Colors.black,
-        ),
-        _buildSettingOption(
-          'Текст красный',
-          textColor == Colors.red,
-          Colors.red,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSettingOption(String title, bool value, Color color) {
-    return ListTile(
-      title: Text(title),
-      trailing: Checkbox(
-        value: value,
-        onChanged: (newValue) {
-          setState(() {
-            if (title.contains('Фон')) {
-              darkBackground = !value;
-            } else {
-              textColor = color;
-            }
-          });
-        },
-      ),
-    );
-  }
-
-  Widget _buildReaderExample() {
-    return Container(
-        width: 312,
-        height: 56,
-        decoration: BoxDecoration(
-          color: darkBackground ? Colors.black : Colors.white,
-          border: Border.all(
-            color: const Color.fromRGBO(235, 235, 235, 1),
-            width: 2,
-          ),
-        ),
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Text(
-            'Текст примера',
-            style:
-                TextStyle(color: textColor, fontFamily: 'Roboto', fontSize: 14),
-          ),
-        ));
   }
 }
--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -7,4 +7,7 @@ class MyColors {
   static const Color grey = Color.fromRGBO(96, 96, 96, 1);
   static const Color mint = Color.fromRGBO(203, 223, 206, 1);
   static const Color beige = Color.fromRGBO(226, 224, 214, 1);
+  static const Color bgWhite = Color.fromRGBO(250, 250, 250, 1);
+  static const Color blackGray = Color.fromRGBO(33, 33, 33, 1);
+  static const Color darkGray = Color.fromRGBO(55, 55, 56, 1);
 }
