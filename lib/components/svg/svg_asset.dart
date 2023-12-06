4e988390f19ac0b2eb61b17eaa9b027d711bb55f
Bogdan Eremin <55001168+kixygod@users.noreply.github.com>
2023-12-06 17:26:48 +0700

    fix(svg): new svg for profile picture

--- lib/components/svg/svg_asset.dart
+++ lib/components/svg/svg_asset.dart
@@ -2,7 +2,7 @@ class SvgAsset {
   SvgAsset._();
 
   static const String merlinLogo = 'assets/images/merlin.svg';
-  static const String merlin = 'assets/images/merlinAnon.svg';
+  static const String merlin = 'assets/images/merlinAnon2.svg';
 
   static const String merlinStart = 'assets/images/merlinStart.svg';
 
